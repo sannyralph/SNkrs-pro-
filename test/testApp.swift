9ede98a4936e98cab21bd6db740b2c7ed1159548
sannyralph <sannylauren@gmail.com>
2023-05-31 09:35:44 -0400

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by SANNI ABDULRAFIU on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
